a56404f02ad125ecb236d313f4a4b0290bf3b29a
Juanma Gómez <juanma1999@gmail.com>
2025-04-10 22:54:58 +0200

    Ciclos

---
+++ Ciclos.java
@@ -0,0 +1,30 @@
+public class Ciclos {
+    
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("Esto es una serie con for: ");
+        for (int i = 1; i <= 10; i++){
+            if(i < 10){
+                System.out.print(i + ", ");
+            }else{
+                System.out.print(i);
+            }
+
+        }
+        i = 1;
+        System.out.println(" ");
+
+        System.out.println("Esto es una serie con while: ");
+
+        while(i <= 10){
+            if(i < 10){
+                System.out.print(i + ", ");
+        }else{
+                System.out.print(i);
+        }
+        i++;
+        }
+        //i = 1;
+        //System.out.println(" ");
+}
+}
